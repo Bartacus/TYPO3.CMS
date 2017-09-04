4767b1add72ec0cfadc30dc5ab04958a1b3f8709
Emily Karisch <emily@karisch.guru>
2017-09-04 14:37:23 +0200

    Never throw an exception if file/folder permissions are already correct

--- typo3/sysext/install/Classes/FolderStructure/AbstractNode.php
+++ typo3/sysext/install/Classes/FolderStructure/AbstractNode.php
@@ -142,9 +142,9 @@ abstract class AbstractNode
     protected function fixPermission(): FlashMessage
     {
         if ($this->isPermissionCorrect()) {
-            throw new Exception(
-                'Permission on ' . $this->getAbsolutePath() . ' are already ok',
-                1366744035
+            return new FlashMessage(
+                '',
+                'Permission on ' . $this->getAbsolutePath() . ' are already ok'
             );
         }
         $result = @chmod($this->getAbsolutePath(), octdec($this->getTargetPermission()));
